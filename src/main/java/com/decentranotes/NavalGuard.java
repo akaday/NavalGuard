a664f5922d909f0f0598f3ba7591e66b954fa5a5
akaday <akadev8@outlook.com>
2024-10-11 04:19:59 +0100

    Add initial NavalGuard class

---
+++ src/main/java/com/decentranotes/NavalGuard.java
@@ -0,0 +1,8 @@
+package com.decentranotes;
+package com.decentranotes;
+
+public class NavalGuard {
+    public static void main(String[] args) {
+        System.out.println("NavalGuard is live!");
+    }
+}
